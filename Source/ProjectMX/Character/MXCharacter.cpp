3985d9213fba4ff5968034cb34e98c2f0dfca03f
Semi <bpwvdf7g55@privaterelay.appleid.com>
2026-06-07 18:37:30 +0900

    test: add MXCharacter - MXCharacter를 추가하였습니다.

---
+++ Source/ProjectMX/Character/MXCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MXCharacter.h"
+
+
+// Sets default values
+AMXCharacter::AMXCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMXCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMXCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMXCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
