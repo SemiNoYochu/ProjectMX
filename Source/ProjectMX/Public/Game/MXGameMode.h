2d47746000136f29608373cb44590048fa164b24
Semi <bpwvdf7g55@privaterelay.appleid.com>
2026-06-09 18:03:07 +0900

    [초기 설정] 파이프라인 클래스 생성 및 스플래시 이미지 변경
    MXGameMode, MXGameState, MXPlayerController, MXCharacter 등등 클래스 추가
    Window 환경 EdSplash, Spalsh 이미지 변경

---
+++ Source/ProjectMX/Public/Game/MXGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MXGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMX_API AMXGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
